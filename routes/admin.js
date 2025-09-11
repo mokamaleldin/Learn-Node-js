997aa7831b513f8a8a9e7dc15a3c5c66b29bc4db
mohamedkeliwa <mhmdalknj496@gmail.com>
2025-09-11 09:56:13 +0300

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
-const express = require('express');
 const path = require('path');
-const rootDir = require('../util/path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-})
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/product', (req, res, next) => {
-    console.log(req.body.title);
-    res.redirect('/');
-})
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,19 @@
-const express = require('express');
 const path = require('path');
-const rootDir = require('../util/path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-})
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
