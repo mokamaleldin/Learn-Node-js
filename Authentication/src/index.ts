d0f51aa7d39c4a1327b3fd304c7b604712e9f0f4
mohamedkeliwa <mhmdalknj496@gmail.com>
2025-09-20 10:53:52 +0300

    update

--- Authentication/src/index.ts
+++ Authentication/src/index.ts
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); // Middleware to parse JSON bodies
 
 
+
 app.use(session({
     secret: process.env.YOUR_SECRET_KEY || '', 
     resave: false,
